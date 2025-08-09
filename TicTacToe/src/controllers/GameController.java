de19cc905f546cd61abb2a398084363d232b6edb
Kevin Ruvunangzia <165159812+KevinRuvunangiza@users.noreply.github.com>
2025-08-09 10:41:38 +0200

    Add GameController and enhance BoardPanel UI
    Introduced GameController to manage game logic and player turns. Updated BoardPanel to use a grid of buttons with action listeners, integrating with GameController for move handling and improved UI layout.

---
+++ TicTacToe/src/controllers/GameController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+public class GameController {
+
+    private String player1Symbol = "X";
+    private String player2Symbol = "O";
+    private String currentPlayerSymbol = player1Symbol;
+    private boolean isGameOver = false;
+
+    public String [][] board = new String[3][3]; 
+
+    public GameController(){
+
+        currentPlayerSymbol = player1Symbol;
+        isGameOver = false;
+
+
+        for(int i = 0; i<3;i++){
+            for(int j = 0; j<3; j++){
+                board[i][j] = " ";
+            }
+        }
+
+        System.out.println("GameController initialized with empty board.");
+        System.out.println(board);
+    }
+
+    public boolean makeMove(int row,int col){
+
+        if (board[row][col].isEmpty()) {
+            board[row][col] = currentPlayerSymbol;
+            switchPlayer();
+            System.out.println("Move made at (" + row + ", " + col + ") by player: " + currentPlayerSymbol);
+            System.out.println("Current board state: "+board[row][col]);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private void switchPlayer(){
+       
+        if (currentPlayerSymbol == player1Symbol) {
+            currentPlayerSymbol = player2Symbol;
+        }else if(currentPlayerSymbol == player2Symbol){
+             currentPlayerSymbol = player1Symbol;
+        }
+    }
+}
--- TicTacToe/src/obj/BoardPanel.java
+++ TicTacToe/src/obj/BoardPanel.java
@@ -1,13 +1,53 @@
 package obj;
 
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class BoardPanel extends JPanel{
-    
-   public BoardPanel(){
-    this.setBackground(Color.DARK_GRAY);
-    
+import controllers.GameController;
+
+public class BoardPanel extends JPanel implements ActionListener {
+
+   public int _rows = 3;
+   public int _cols = 3;
+   public int _hGap = 5;
+   public int _vGap = 5;
+   public int _numOfGrids = _rows * _cols;
+
+   JButton[][] gridButtons = new JButton[_rows][_cols];
+
+   public BoardPanel() {
+      this.setBackground(Color.DARK_GRAY);
+      this.setLayout(new GridLayout(_rows, _cols, _hGap, _vGap));
+
+      for (int i = 0; i < _rows; i++) {
+         for (int j = 0; j < _cols; j++) {
+
+            gridButtons[i][j] = new JButton();
+
+            gridButtons[i][j].addActionListener(e -> actionPerformed(e));
+            gridButtons[i][j].setActionCommand(i + "," + j);
+            gridButtons[i][j].setFont(gridButtons[i][j].getFont().deriveFont(24f));
+            gridButtons[i][j].setForeground(Color.BLACK);
+            gridButtons[i][j].setBackground(Color.white);
+            gridButtons[i][j].setOpaque(true);
+            gridButtons[i][j].setBorderPainted(false);
+            this.add(gridButtons[i][j]);
+         }
+      }
+   }
+
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      GameController gameController = new GameController();
+      String command = e.getActionCommand();
+      String[] indices = command.split(",");
+      int row = Integer.parseInt(indices[0]);
+      int col = Integer.parseInt(indices[1]);
+      boolean moveMade = gameController.makeMove(row, col);
    }
 }
